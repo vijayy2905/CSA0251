1e4a45d43d2845e7c228bb5e38cfe149c468d766
Vijay <192565102.simats@saveetha.com>
2026-04-14 11:38:04 +0530

    Create Find Days using switch.c

---
+++ Find Days using switch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the number between 1 to 7: ");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid number");
+    }
+}
